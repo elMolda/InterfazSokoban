eebea1573d5df9615658ee3f52db19e4c0118402
Andres Contreras <andres_c8@outlook.com>
2018-11-02 16:54:43 -0500

    se llenan los atributos

--- Sokoban.py
+++ Sokoban.py
@@ -4,33 +4,33 @@ class Board:
     Data = [[]]
     Width = 0
     Height = 0
-
+    playerpos = [0,0]
 
     ## ---------------------------------------------------------------------
 
     def __init__( self, arch ):
          print(const.CAJA)
-         cont = 0
          with open('tablero.txt') as f:
             lines = f.readlines()
-            for line in lines:
-                if(cont == 0):
-                    print("WIDTH ES: ",line)
-                    self.Width = line
-                    cont +=1
-                elif(cont == 1):
-                    self.Height = line
-                    cont +=1
-                else:
-                    self.Data[cont-2] = line
-                    cont+=1
-                print("==============")
-                print(line)
+            self.Width = int(lines[0])
+            self.Height = int(lines[1])
+            self.llenarMatriz(lines)    
          f.close()
          print("Width: ",self.Width)
          print("Height: ",self.Height)
+         print("jugador: ", self.playerpos)
          print("tabla")
          print(self.Data)
+
+    def llenarMatriz(self,lines):
+        print("W: ",self.Width, "H: ",self.Height)
+
+        self.Data = [ [ (0) for j in range(self.Height)] for i in range(self.Width) ]
+        for i in range (2,self.Height+2):
+            for j in range (0,self.Width):
+                self.Data[i-2][j] = lines[i][j]
+                if(self.Data[i-2][j] == const.JUGADOR or self.Data[i-2][j] == const.JUGADORM):
+                    self.playerpos = [i-2,j]
 
     def mover (self,direccion):
         print("B")
